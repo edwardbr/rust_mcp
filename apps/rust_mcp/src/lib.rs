6886383a32fa3dbefa1b0b91c97146fb52d7b0a2
Edward Boggis-Rolfe <edward@boggis-rolfe.com>
2025-07-31 11:34:03 +0100

    Try GET

--- apps/rust_mcp/src/lib.rs
+++ apps/rust_mcp/src/lib.rs
@@ -78,7 +78,7 @@ impl Guest for Component {
 
         klave::notifier::send_string(&url);
         let https_request = Request::builder()
-            .method("POST")
+            .method("GET")
             .uri(url)
             .header("Content-Type", "application/json")
             .body(String::from(""))
